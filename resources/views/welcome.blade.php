6a026c0995129073b428c5a42637f0aac4862bfe
SaiSoeSan <33777412+SaiSoeSan@users.noreply.github.com>
2025-04-23 18:15:39 -0500

    jobs crud is born

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Job Tracker</title>
+</head>
+<body>
+    <h4>Welcome to Job Tracker Backend</h4>
+    <p>This is the backend application for job tracker application</p>
+
+</body>
+</html>
